fb022caffb2bd1f844500a87ae4d95c02b8d98e0
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2023-10-27 14:57:30 -0700

    Displaying Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import useGenres, { Genre } from "../hooks/useGenres";
+import { List, ListItem, HStack, Image, Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 
 function GenreList() {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data.map((genre: Genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            ></Image>
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 }
 
